eced28a4f3ef7f3fa213aeef053486bbc02d070b
Danilo Moura <danilomouraa@icloud.com>
2021-04-19 15:27:43 -0300

    Test Create Rental

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,13 +1,57 @@
-// import { CreateRentalUseCase } from "./CreateRentalUseCase";
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
-// let createRentalUseCase: CreateRentalUseCase;
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
-// describe("Create Rental", () => {
-//   beforeEach(() => {
-//     createRentalUseCase = new CreateRentalUseCase();
-//   });
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
 
-//   it("Should be able to create a new Rental", async () => {
-//     await createRentalUseCase.execute();
-//   });
-// });
+describe("Create Rental", () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it("Should be able to create a new Rental", async () => {
+    const rental = await createRentalUseCase.execute({
+      user_id: "12345",
+      car_id: "121212",
+      expected_return_date: new Date(),
+    });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("Should not be able to create a new Rental if there is another open to the same user", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "12345",
+        car_id: "121212",
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: "12345",
+        car_id: "121212",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to create a new Rental if there is another open to the same car", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "123",
+        car_id: "test",
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: "XXX",
+        car_id: "test",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,11 +1,48 @@
-// interface IRequest {
-//   user_id: string;
-//   car_id: string;
-//   expected_return_date: Date;
-// }
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
 
-// class CreateRentalUseCase {
-//   async execute(): Promise<void>;
-// }
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
 
-// export { CreateRentalUseCase };
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    // verifica se o carro já está em uso em outro aluguel
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable");
+    }
+    // verifica se esse user já está com algum aluguel aberto
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppError("There's a rental in progress for user!");
+    }
+
+    // [X] O alguel deve ter duração mínima de 24 horas
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
